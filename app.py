11093f2d93bd4f9a6db1f9588545a5656ef02f4e
isdicely <75281465+isdicely@users.noreply.github.com>
2021-06-01 22:43:42 -0700

    set up engine

--- app.py
+++ app.py
@@ -6,8 +6,21 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
+
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///hawaii.sqlite", echo=False)
+
+# reflect an existing database into a new model
+    # creates set of clases for each table in hawaii.sqlite
+Base = automap_base()
+
+# reflect the tables (defines classes that represent data in the table)
+Base.prepare(engine, reflect=True)
+
 # Set up routes with Flask
 app = Flask(__name__)
+# Index route
+
 
 @app.route("/")
 def index():
@@ -21,3 +34,17 @@ def index():
         f"<li>/api/v1.0/temp/start/end</li>"
         f"</ul></p>"
     )
+
+    # precipation route
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+    return(
+
+    )
+
+
+if __name__ == '__main__':
+    app.run()
