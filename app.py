5b47840f8362078c868b58abebe1fe416fd373eb
isdicely <75281465+isdicely@users.noreply.github.com>
2021-06-01 22:35:30 -0700

    added routes

--- app.py
+++ app.py
@@ -5,3 +5,19 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+
+# Set up routes with Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return(
+        f"<h1>Hawaii Climate Analysis API</h1>"
+        f"<p>Available Routes:"
+        f"<ul>"
+        f"<li>/api/v1.0/precipitation</li>"
+        f"<li>/api/v1.0/stations</li>"
+        f"<li>/api/v1.0/tobs</li>"
+        f"<li>/api/v1.0/temp/start/end</li>"
+        f"</ul></p>"
+    )
